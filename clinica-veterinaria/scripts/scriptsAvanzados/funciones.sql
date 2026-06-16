05c3e7752aa393e1283248746f3ed42caa6c96fc
Astrid00999 <00091525@uca.edu.sv>
2026-06-16 11:31:59 -0600

    :art: style(): Mejoramiento de script de funciones

--- clinica-veterinaria/scripts/scriptsAvanzados/funciones.sql
+++ clinica-veterinaria/scripts/scriptsAvanzados/funciones.sql
@@ -1,33 +1,33 @@
+--================================================================================
+--                                    FUNCIONES
+--================================================================================
 
---Función que retorna la edad del paciente mediante la fecha de nacimiento.
 
---Creación de la función
-create or replace function fn_get_edad(f_fecha_nacimiento date)
-	returns int
-	language plpgsql
-as $$
-
---Declaración de variable c_edad, guardará la edad calculada.
-declare
-c_edad bigint;
-begin
-
---Calcula directamente con el parámetro recibido
-	c_edad = extract(year from age(current_date, f_fecha_nacimiento)); 
+-- 1- Función que calcula la edad del paciente.
+---------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION fn_get_edad(f_fecha_nacimiento date)
+	RETURNS int
+	LANGUAGE plpgsql
+AS $$
+DECLARE
+	c_edad bigint;
+BEGIN
+	c_edad = EXTRACT(YEAR FROM age(current_date, f_fecha_nacimiento)); 
    
 --Retorna la edad calculada, si es NULL retorna 0
-	return coalesce(c_edad,0);
+RETURN coalesce(c_edad,0);
 
-end;
+END;
 $$;
 
 -- Uso de la función en una consulta SELECT
-select nombre_m, fecha_nacimiento, fn_get_edad(fecha_nacimiento) as edad from mascotas;
+SELECT nombre_m, fecha_nacimiento, fn_get_edad(fecha_nacimiento) AS edad FROM mascotas;
 
 
---Función que retorna historial clínico de una mascota. 
-create or replace function fn_get_historial_medico(id int)
-returns table(
+--2. Función que retorna historial clínico de una mascota. 
+--------------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION fn_get_historial_medico(id int)
+RETURNS TABLE(
     nombre_ma text,
     edad int,
     peso numeric(6,2),
@@ -37,11 +37,11 @@ returns table(
     tratamiento text,
     medicamentos_recetados text
 )
-language plpgsql
-as $$
-begin
-    return query
-    select
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN query
+    SELECT
         m.nombre_m::text,
         fn_get_edad(m.fecha_nacimiento)::int,
         m.peso_m,
@@ -49,29 +49,32 @@ begin
 		v.nombre_v::text,
         d.descripcion_d::text,
         t.descripcion_t::text,
+--Función de agregada que concatena valores de varias filas en una sola cadena.
 		string_agg(
             me.nombre_m || ' (' || dt.dosis_dt || ')',
             ', '
         )::text as medicamentos_recetados
-    from mascotas m
+    FROM mascotas m
 
-    join citas c on c.id_mascota = m.id_mascota
-	join veterinario v on v.id_veterinario = c.id_veterinario
-    join diagnosticos d on d.id_cita = c.id_cita
-    join tratamiento t on t.id_diagnostico = d.id_diagnostico
-    join detalles_tratamientos dt on dt.id_tratamiento = t.id_tratamiento
-    join medicamentos me on me.id_medicamento = dt.id_medicamento
-    where m.id_mascota = id
-group by
- m.nombre_m,
+    JOIN citas c on c.id_mascota = m.id_mascota
+	JOIN veterinario v on v.id_veterinario = c.id_veterinario
+    JOIN diagnosticos d on d.id_cita = c.id_cita
+    JOIN tratamiento t on t.id_diagnostico = d.id_diagnostico
+    JOIN detalles_tratamientos dt on dt.id_tratamiento = t.id_tratamiento
+    JOIN medicamentos me on me.id_medicamento = dt.id_medicamento
+    WHERE m.id_mascota = id
+GROUP BY
+ 	m.nombre_m,
     m.fecha_nacimiento,
     m.peso_m,
     c.fecha_c,
     d.descripcion_d,  
     t.descripcion_t,
 	v.nombre_v;
-end;
+END;
 $$;
 
+--Consulta select para mostrar el historial médico. 
+SELECT * FROM fn_get_historial_medico(3);
 
-select * from fn_get_historial_medico(3);
+--=====================================================================================
